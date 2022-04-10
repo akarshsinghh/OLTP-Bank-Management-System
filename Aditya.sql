1f9c24410617ffd167d8c26bb201b6bcdb4d1a47
AdityaTilak <atilak997@gmail.com>
2022-04-10 14:32:26 -0400

    Creating tables CUSTOMER & WORKS_WITH

---
+++ Aditya.sql
@@ -0,0 +1,25 @@
+DROP TABLE CUSTOMER;
+CREATE TABLE CUSTOMER(
+Customer_ID INT PRIMARY KEY,
+First_Name VARCHAR(20) NOT NULL,
+Last_Name VARCHAR(20) NOT NULL,
+DOB Date,
+Works_at VARCHAR(20),
+Sex VARCHAR(1) CONSTRAINT Genders CHECK(Sex IN ('M','F')),
+Apt_No VARCHAR(10),
+Street VARCHAR(10),
+City VARCHAR(10),
+CState VARCHAR(2),
+Zipcode int CONSTRAINT Zip_Length CHECK (LENGTH(Zipcode)=5),
+Phone int CONSTRAINT Phone_Length CHECK(LENGTH(Phone)=10)
+);
+
+DROP TABLE WORKS_WITH;
+CREATE TABLE WORKS_WITH(
+Customer_ID INT NOT NULL,
+Employee_ID INT NOT NULL,
+Relationship varchar(20)
+);
+
+
+
