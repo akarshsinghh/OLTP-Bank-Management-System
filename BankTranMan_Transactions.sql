b45776bb2d4451c3ed6f47a538b96d2cd3ffcfed
Akarsh Singh <akarshsinghh@gmail.com>
2022-04-14 23:45:51 -0400

    Changes for successful merge

--- BankTranMan_Transactions.sql
+++ BankTranMan_Transactions.sql
@@ -1,89 +1,46 @@
-drop table TRANSACTIONS;
-CREATE TABLE TRANSACTIONS(
-
-    Tansaction_ID   int not null primary key,
-    Debit_Account   number(30),
-    Credit_Account  number(30),
-    IFSC_Code_Debit varchar(11),
-    IFSC_Code_Credit varchar (11),
-    Amount          Decimal(10,2) not null constraint amt_check check( Amount > 0),
-    Transaction_date date,
-    Message         varchar(100),
-    Status          varchar(20)
-
-);
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940301, 13719713158835300, 1086643758945400, 'DMDD0454500','DMDD0101022','20.00',DATE '2021-03-08','sent $20', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
-(1103876940301, 53153834006064000, 3690859741294280, 'DMDD0101022','DMDD0454500','40.00',DATE '2021-04-11','Amount $40 Transferred', 'Success');
-
+(1103876940301, 53153834006064000, 3719713158855555, 'DMDD0101022','DMDD0454500','40.00',DATE '2021-04-11','Amount $40 Transferred', 'Success');
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940302, 37530666568966100, 70048841700216300, 'DMDD0333100','DMDD0454500','250.00',DATE '2022-10-10','sent $250', 'Pending');
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940303, 89135767380499400, 12494980148173100, 'DMDD0101022','DMDD0333100','50.00',DATE '2020-11-08','sent $50', 'Success');
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940304, 8744336085399580, 64289489845768000, 'DMDD0101022','DMDD0454500','330.00',DATE '2021-04-08','sent $330', 'Success');
-
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940305, 70149340835549500, 13719713158835300, 'DMDD0333100','DMDD0454500','150.00',DATE '2020-05-18','sent $150', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
-(1203876940306, 13719713158835300, 1086643758945400, 'DMDD0454500','DMDD0101022','120.00',DATE '2021-02-29','sent $120', 'Success');
-
+(1203876940306,9560713158835400 ,9009713158835400 , 'DMDD0454500','DMDD0101022','120.00',DATE '2021-02-23','sent $120', 'Success');
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940307, 53153834006064000, 3690859741294280, 'DMDD0101022','DMDD0454500','400.00',DATE '2020-08-05','Amount $400 Transferred', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940308, 37530666568966100, 70048841700216300, 'DMDD0333100','DMDD0454500','500.00',DATE '2022-01-18','sent $500', 'Success');
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940309, 89135767380499400, 12494980148173100, 'DMDD0101022','DMDD0333100','350.00',DATE '2021-07-28','sent $350', 'Success');
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940310, 8744336085399580, 64289489845768000, 'DMDD0101022','DMDD0454500','444.00',DATE '2021-10-04','sent $444', 'Success');
-
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940311, 70149340835549500, 13719713158835300, 'DMDD0333100','DMDD0454500','100.00',DATE '2021-02-11','sent $100', 'Success');
-
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940312, 13719713158835300, 1086643758945400, 'DMDD0454500','DMDD0101022','1000.00',DATE '2021-03-08','sent $1000', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
-(1103876940313, 53153834006064000, 3690859741294280, 'DMDD0101022','DMDD0454500','700.00',DATE '2021-02-29','Amount $700 Transferred', 'Success');
-
+(1103876940313, 53153834006064000, 3690859741294280, 'DMDD0101022','DMDD0454500','700.00',DATE '2021-08-29','Amount $700 Transferred', 'Success');
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940314, 37530666568966100, 70048841700216300, 'DMDD0333100','DMDD0454500','750.00',DATE '2021-10-10','sent $750', 'Pending');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940315, 89135767380499400, 12494980148173100, 'DMDD0101022','DMDD0333100','55.00',DATE '2020-11-09','sent $55', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940316, 8744336085399580, 64289489845768000, 'DMDD0101022','DMDD0454500','360.00',DATE '2021-06-03','sent $360', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940317, 70149340835549500, 13719713158835300, 'DMDD0333100','DMDD0454500','66.00',DATE '2020-09-03','sent $66', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1203876940318, 13719713158835300, 1086643758945400, 'DMDD0454500','DMDD0101022','450.00',DATE '2021-08-01','sent $450', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940319, 53153834006064000, 3690859741294280, 'DMDD0101022','DMDD0454500','600.00',DATE '2021-01-01','Amount $600 Transferred', 'Success');
-
 insert into TRANSACTIONS(Tansaction_ID, Debit_Account,Credit_Account,IFSC_Code_Debit,IFSC_Code_Credit, Amount,  Transaction_date,Message,Status) Values
 (1103876940320, 37530666568966100, 70048841700216300, 'DMDD0333100','DMDD0454500','60.00',DATE '2022-04-13','sent $60', 'Success');
+
 
 select * from TRANSACTIONS;
 
